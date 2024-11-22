669ca9cbaacdad91b81439351e1b6454173a8117
Alice131419 <manuscarlet@yahoo.com>
2024-11-22 16:07:46 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,21 +4,23 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/Alice131419" target="blank">
-          Alice Petcu
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Alice131419/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project was coded by{" "}
+          <a href="https://github.com/Alice131419" target="blank">
+            Alice Petcu
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Alice131419/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
