43eb9d0f3e34c5192b8babc8a641c1510bc86376
Alice131419 <manuscarlet@yahoo.com>
2024-11-22 15:31:40 +0200

    Added the GitHub URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/Alice131419" target="blank">
+          Alice Petcu
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Alice131419/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
