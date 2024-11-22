b650aa0b8b5d5fbf177a2456378cd543d4c7e4e6
Alice131419 <manuscarlet@yahoo.com>
2024-11-22 15:50:15 +0200

    updated GitHub link

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ export default function App() {
         <a
           href="https://github.com/Alice131419/react-weather-app"
           target="_blank"
+          rel="noreferrer"
         >
           open-sourced on GitHub
         </a>
