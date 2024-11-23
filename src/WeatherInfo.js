7e48686805d01e98140c7c373bd8ee5258bf41bc
Alice131419 <manuscarlet@yahoo.com>
2024-11-23 17:21:23 +0200

    Updated WeatherInfo

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -4,32 +4,32 @@ import FormattedDate from "./FormattedDate";
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
-      <h1>{props.weatherData.city} </h1>
+      <h1>{props.data.city} </h1>
       <ul>
         <li>
           {" "}
-          <FormattedDate date={props.weatherData.date} />
+          <FormattedDate date={props.data.date} />
         </li>
-        <li className="text-capitalize">{props.weatherData.description} </li>
+        <li className="text-capitalize">{props.data.description} </li>
       </ul>
       <div className="row mt-3">
         <div className="col-6">
           <div className="clearfix">
             <img
-              src={props.weatherData.iconUrl}
-              alt={props.weatherData.description}
+              src={props.data.iconUrl}
+              alt={props.data.description}
               className="float-left"
             />
             <span className="temperature">
-              {Math.round(props.weatherData.temperature)}
+              {Math.round(props.data.temperature)}
             </span>
             <span className="unit">°C</span>
           </div>
         </div>
         <div className="col-6">
           <ul>
-            <li>Humidity:{props.weatherData.humidity} </li>
-            <li>Wind:{props.weatherData.wind} km/h</li>
+            <li>Humidity:{props.data.humidity} </li>
+            <li>Wind:{props.data.wind} km/h</li>
           </ul>
         </div>
       </div>
